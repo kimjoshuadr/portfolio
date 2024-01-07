5a86bcfcbfad94d05fdf2f39b6e80a4906a387f3
Kim Joshua Delos Reyes <kimjoshuadr@gmail.com>
2024-01-07 09:59:57 +0800

    added active section scroll effect and reveal animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-/**--NAV BAR-- */
 // button click
 document.querySelector('.uil-bars').addEventListener('click', () => {
     const navBar = document.querySelector('.nav-bar')
     navBar.classList.toggle('hidden')
 })
-// header scroll shadow
+// scroll effect
 document.addEventListener('scroll', () => {
-    let lastKnownScrollPosition = window.scrollY
+    // scroll header shadow
+    const lastKnownScrollPosition = window.scrollY
     const navHeader = document.getElementById('header')
 
     if (lastKnownScrollPosition > 50) {
@@ -18,4 +18,62 @@ document.addEventListener('scroll', () => {
         navHeader.style.height = "90px"
         navHeader.style.lineHeight = "90px"
     }
+    // active scroll
+    const sections = document.querySelectorAll('section[id]')
+    sections.forEach(currentSection => {
+        const sectionTop = currentSection.offsetTop - 75
+        const sectionHeight = currentSection.offsetHeight
+        const sectionID = currentSection.getAttribute('id')
+        if (lastKnownScrollPosition > sectionTop && lastKnownScrollPosition <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav-list a[href*= ${sectionID}]`).classList.add('active-section')
+        } else {
+            document.querySelector(`.nav-list a[href*= ${sectionID}]`).classList.remove('active-section')
+        }
+    })
 })
+// typing effect
+var typingEffect = new Typed('.animated-text',{
+    strings: ["Kim Joshua","a Developer","a Freelancer","an Engineer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
+// scroll reveal animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.hero-name',{})
+sr.reveal('.hero-text',{delay: 100})
+sr.reveal('.hero-text-info',{delay: 200})
+sr.reveal('.hero-text-button',{delay: 200})
+sr.reveal('.social-icons',{delay: 200})
+sr.reveal('.hero-image-section',{delay: 300})
+
+sr.reveal('.project-card',{interval: 200})
+
+sr.reveal('.top-header',{})
+
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+
+srRight.reveal('.skills-info',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
